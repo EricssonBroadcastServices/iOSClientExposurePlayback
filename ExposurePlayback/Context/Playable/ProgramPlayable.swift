001fcf0ea06cb3601821d5d7eda5ee258f068114
Fredrik Sjöberg <fredrik.sjoberg@webstep.se>
2018-10-29 14:33:21 +0100

    Use program play endpoint without channelId

--- ExposurePlayback/Context/Playable/ProgramPlayable.swift
+++ ExposurePlayback/Context/Playable/ProgramPlayable.swift
@@ -10,7 +10,8 @@ import Foundation
 import Exposure
 
 internal protocol ProgramEntitlementProvider {
-    func requestEntitlement(programId: String, channelId: String, using sessionToken: SessionToken, in environment: Environment, callback: @escaping (PlaybackEntitlement?, ExposureError?, HTTPURLResponse?) -> Void)
+    func requestEntitlement(programId: String, using sessionToken: SessionToken, in environment: Environment, callback: @escaping (PlaybackEntitlement?, ExposureError?, HTTPURLResponse?) -> Void)
+    
 }
 
 /// Defines a `Playable` for the specific program
@@ -24,33 +25,19 @@ public struct ProgramPlayable: Playable {
     internal var entitlementProvider: ProgramEntitlementProvider = ExposureEntitlementProvider()
     
     internal struct ExposureEntitlementProvider: ProgramEntitlementProvider {
-        func requestEntitlement(programId: String, channelId: String, using sessionToken: SessionToken, in environment: Environment, callback: @escaping (PlaybackEntitlement?, ExposureError?, HTTPURLResponse?) -> Void) {
-            let entitlement = Entitlement(environment: environment,
-                                          sessionToken: sessionToken)
-                .program(programId: programId,
-                         channelId: channelId)
+        func requestEntitlement(programId: String, using sessionToken: SessionToken, in environment: Environment, callback: @escaping (PlaybackEntitlement?, ExposureError?, HTTPURLResponse?) -> Void) {
+            let params: [String: String] = [
+                "drm":"FAIRPLAY",
+                "format":"HLS"
+            ]
             
-            entitlement
-                .request()
+            ExposureApi<PlaybackEntitlement>(environment: environment, endpoint: "/entitlement/program/\(programId)/play", parameters: params, method: .post, sessionToken: sessionToken)
+                .request(encoding: JSONEncoding())
                 .validate()
                 .response{
-                    if let error = $0.error {
-                        // Workaround until EMP-10023 is fixed
-                        if case let .exposureResponse(reason: reason) = error, (reason.httpCode == 403 && reason.message == "NO_MEDIA_FOR_PROGRAM") {
-                            entitlement
-                                .use(drm: "UNENCRYPTED")
-                                .request()
-                                .validate()
-                                .response{ callback($0.value, $0.error, $0.response) }
-                        }
-                        else {
-                            callback($0.value, $0.error, $0.response)
-                        }
-                    }
-                    else {
-                        callback($0.value, $0.error, $0.response)
-                    }
-                }
+                    callback($0.value, $0.error, $0.response)
+                    
+            }
         }
     }
 }
@@ -69,7 +56,7 @@ extension ProgramPlayable {
     /// - parameter sessionToken: `SessionToken` validating the user
     /// - parameter callback: Closure called on request completion
     public func prepareSource(environment: Environment, sessionToken: SessionToken, callback: @escaping (ExposureSource?, ExposureError?) -> Void) {
-        entitlementProvider.requestEntitlement(programId: assetId, channelId: channelId, using: sessionToken, in: environment) { entitlement, error, response in
+        entitlementProvider.requestEntitlement(programId: assetId, using: sessionToken, in: environment) { entitlement, error, response in
             if let value = entitlement {
                 let source = ProgramSource(entitlement: value, assetId: self.assetId, channelId: self.channelId)
                 source.response = response
@@ -84,7 +71,7 @@ extension ProgramPlayable {
 
 extension ProgramPlayable {
     public func prepareSourceWithResponse(environment: Environment, sessionToken: SessionToken, callback: @escaping (ExposureSource?, ExposureError?, HTTPURLResponse?) -> Void) {
-        entitlementProvider.requestEntitlement(programId: assetId, channelId: channelId, using: sessionToken, in: environment) { entitlement, error, response in
+        entitlementProvider.requestEntitlement(programId: assetId, using: sessionToken, in: environment) { entitlement, error, response in
             if let value = entitlement {
                 let source = ProgramSource(entitlement: value, assetId: self.assetId, channelId: self.channelId)
                 source.response = response
