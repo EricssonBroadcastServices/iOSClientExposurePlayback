21d81250ee2f56f6a666e6a37660b351c1df7edb
udayasri <udayasricl@gmail.com>
2021-11-17 13:51:46 +0100

    Remove unused logs

--- ExposurePlayback/Services/AdService/AdServiceEventProvider.swift
+++ ExposurePlayback/Services/AdService/AdServiceEventProvider.swift
@@ -18,15 +18,12 @@ internal class AdServiceEventProvider: AnalyticsProvider, TimedMetadataProvider,
     }
     
     func onCreated<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onCreated ")
     }
     
     func onPrepared<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onPrepared ")
     }
     
     func onReady<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onReady ")
         adService.playbackReady()
     }
     
@@ -35,22 +32,18 @@ internal class AdServiceEventProvider: AnalyticsProvider, TimedMetadataProvider,
     }
     
     func onPaused<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onPaused ")
         adService.playbackPaused()
     }
     
     func onResumed<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onResumed ")
         adService.playbackResumed()
     }
     
     func onAborted<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onAborted ")
         adService.playbackEnded()
     }
     
     func onCompleted<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onCompleted ")
         adService.playbackEnded()
     }
     
@@ -65,12 +58,10 @@ internal class AdServiceEventProvider: AnalyticsProvider, TimedMetadataProvider,
     }
     
     func onBufferingStarted<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onBufferingStarted ")
         adService.playbackBufferingStarted()
     }
     
     func onBufferingStopped<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onBufferingStopped ")
         adService.playbackBufferingEnded()
     }
     
@@ -79,7 +70,7 @@ internal class AdServiceEventProvider: AnalyticsProvider, TimedMetadataProvider,
     }
     
     func onDurationChanged<Tech, Source>(tech: Tech, source: Source) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onDurationChanged ")
+
     }
     
     func onWarning<Tech, Source, Context>(tech: Tech, source: Source?, warning: PlayerWarning<Tech, Context>) where Tech : PlaybackTech, Source : MediaSource, Context : MediaContext {
@@ -87,70 +78,10 @@ internal class AdServiceEventProvider: AnalyticsProvider, TimedMetadataProvider,
     }
     
     func onTimedMetadataChanged<Tech, Source>(source: Source?, tech: Tech, metadata: [AVMetadataItem]?) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onTimedMetadataChanged ")
-        
-        /* if let timeMedata = metadata {
-            for item in timeMedata {
-                        switch item.commonKey {
-
-                        case .commonKeyAlbumName?:
-                            print("AlbumName: \(item.value!)")
-                        case .commonKeyArtist?:
-                            print("Artist: \(item.value!)")
-                        case .commonKeyArtwork?:
-                            print("Artwork: \(item.value!)")
-                        case .commonKeyAuthor?:
-                            print("Author: \(item.value!)")
-                        case .commonKeyContributor?:
-                            print("Contributor: \(item.value!)")
-                        case .commonKeyCopyrights?:
-                            print("Copyrights: \(item.value!)")
-                        case .commonKeyCreationDate?:
-                            print("CreationDate: \(item.value!)")
-                        case .commonKeyCreator?:
-                            print("creator: \(item.value!)")
-                        case .commonKeyDescription?:
-                            print("Description: \(item.value!)")
-                        case .commonKeyFormat?:
-                            print("Format: \(item.value!)")
-                        case .commonKeyIdentifier?:
-                            print("Identifier: \(item.value!)")
-                        case .commonKeyLanguage?:
-                            print("Language: \(item.value!)")
-                        case .commonKeyMake?:
-                            print("Make: \(item.value!)")
-                        case .commonKeyModel?:
-                            print("Model: \(item.value!)")
-                        case .commonKeyPublisher?:
-                            print("Publisher: \(item.value!)")
-                        case .commonKeyRelation?:
-                            print("Relation: \(item.value!)")
-                        case .commonKeySoftware?:
-                            print("Software: \(item.value!)")
-                        case .commonKeySubject?:
-                            print("Subject: \(item.value!)")
-                        case .commonKeyTitle?:
-                            print("Title: \(item.value!)")
-                        case .commonKeyType?:
-                            print("Type: \(item.value!)")
-
-                        case .id3MetadataKeyAlbumTitle?:
-                            print("id3MetadataKeyAlbumTitle: \(item.value!)")
-
-                        default:
-                            print("other data: \(item.value!)")
-                        }
-                    }
-        } */
-
-        
-        
-        
         adService.playbackTimedMetadata(metaData: metadata)
     }
     
     func onSourcePreparationAbandoned<Tech, Source>(ofSource mediaSource: Source, byTech tech: Tech) where Tech : PlaybackTech, Source : MediaSource {
-        // print(" AdServiceEventProvider onSourcePreparationAbandoned ")
         adService.playbackEnded()
     }
 }
