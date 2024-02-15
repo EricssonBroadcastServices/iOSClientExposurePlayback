8ead462758f887ba88e08d407f25968ed2c44ca0
Udaya Sri Senarathne <udayasricl@gmail.com>
2024-02-15 17:00:39 +0100

    - Update URLPlayable to take url & player as parameters - Add  new interface `startPlayback(urlPlayable: URLPlayable)` , client developers can use this to pass URLPlayable to play any stream.

---
+++ Sources/iOSClientExposurePlayback/Context/Components/HLSNative+ExposureContext+URLPlayback.swift
@@ -0,0 +1,22 @@
+//
+//  HLSNative+ExposureContext+URLPlayback.swift
+//
+//
+//  Created by Udaya Sri Senarathne on 2024-02-15.
+//
+
+import Foundation
+import iOSClientPlayer
+import iOSClientExposure
+
+extension Player where Tech == HLSNative<ExposureContext> {
+    
+    public func startPlayback(urlPlayable: URLPlayable) {
+        
+        let tech = HLSNative<ManifestContext>()
+        let context = ManifestContext()
+        
+        urlPlayable.player.stream(url: urlPlayable.url)
+
+    }
+}
--- Sources/iOSClientExposurePlayback/Context/Playable/URLPlayable.swift
+++ Sources/iOSClientExposurePlayback/Context/Playable/URLPlayable.swift
@@ -7,58 +7,15 @@
 
 import Foundation
 import iOSClientExposure
+import iOSClientPlayer
 
-public struct URLPlayable: Playable {
+public struct URLPlayable {
+    
     let url: URL
-    public var assetId: String
+    var player: Player<HLSNative<ManifestContext>>!
     
-    public init(url: URL) {
+    public init(url: URL, player: Player<HLSNative<ManifestContext>>) {
         self.url = url
-        self.assetId = url.absoluteString
-    }
-    
-    public func prepareSource(
-        environment: iOSClientExposure.Environment,
-        sessionToken: iOSClientExposure.SessionToken,
-        adsOptions: iOSClientExposure.AdsOptions?,
-        adobePrimetimeMediaToken: String?,
-        materialProfile: String?,
-        customAdParams: [String : Any]?,
-        metadataIdentifiers: [String]?,
-        deviceMake: String?,
-        deviceModel: String?,
-        callback: @escaping (ExposureSource?, iOSClientExposure.ExposureError?) -> Void
-    ) {
-        let ent = PlaybackEntitlement(
-            assetId: nil,
-            accountId: nil,
-            audioOnly: nil,
-            playTokenExpiration: "",
-            mediaLocator: self.url,
-            playSessionId: "",
-            live: false,
-            ffEnabled: true,
-            timeshiftEnabled: true,
-            rwEnabled: true,
-            airplayBlocked: false,
-            playToken: "",
-            fairplay: nil,
-            licenseExpiration: "",
-            licenseExpirationReason: "",
-            licenseActivation: "",
-            entitlementType: "",
-            minBitrate: nil,
-            maxBitrate: nil,
-            maxResHeight: nil,
-            mdnRequestRouterUrl: nil,
-            lastViewedOffset: nil,
-            lastViewedTime: nil,
-            liveTime: nil,
-            productId: nil,
-            adMediaLocator: nil
-        )
-        
-        let urlSource = URLSource(entitlement: ent, assetId: self.assetId, streamingInfo: nil)
-        callback(urlSource, nil)
+        self.player = player
     }
 }
