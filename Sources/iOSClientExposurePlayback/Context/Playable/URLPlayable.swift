5694d1defd7074c6a2d0f1875f92393923891153
Robert Pelka <158444423+robert-pelka@users.noreply.github.com>
2024-02-16 14:18:44 +0100

    [EMP-21047] feat: Add support to play streams with URL as source (#117)

---
+++ Sources/iOSClientExposurePlayback/Context/Playable/URLPlayable.swift
@@ -0,0 +1,16 @@
+//
+//  URLPlayable.swift
+//
+//
+//  Created by Robert Pelka on 14/02/2024.
+//
+
+import Foundation
+
+public struct URLPlayable {
+    public let url: URL
+    
+    public init(url: URL) {
+        self.url = url
+    }
+}
